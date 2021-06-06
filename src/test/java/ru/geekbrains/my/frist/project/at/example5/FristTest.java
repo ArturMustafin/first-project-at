8ea33eeca4da7ef83f3c22fa59876ca9b5542169
artur <artur5mustafin@gmail.com>
2021-06-06 23:47:50 +0500

    fix pom
    issue 323

--- src/test/java/ru/geekbrains/my/frist/project/at/example5/FristTest.java
+++ src/test/java/ru/geekbrains/my/frist/project/at/example5/FristTest.java
@@ -10,6 +10,7 @@ import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import ru.geekbrains.my.frist.project.at.example2.Page;
 
 
@@ -18,8 +19,23 @@ import ru.geekbrains.my.frist.project.at.example2.Page;
 public class FristTest {
 
     @BeforeEach
-    void setUP() {
-        Configuration.browserCapabilities.setCapability("enableVNC", true);
+    void setUp() {
+        //Url удалённого веб драйвера
+        Configuration.remote = "http://localhost:4444/wd/hub";
+        //Определяем какой браузер будем использовать
+        Configuration.browser = "chrome";
+        //Размер окна браузера
+        Configuration.browserSize = "1920x1080";
+        //Создаём объект класса DesiredCapabilities, используется как настройка  вашей конфигурации с помощью пары ключ-значение
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        //Включить поддержку отображения экрана браузера во время выполнения теста
+        capabilities.setCapability("enableVNC", true);
+        //Включение записи видео в процессе выполнения тестов
+        capabilities.setCapability("enableVideo", true);
+        //Переопределяем Browser capabilities
+        Configuration.browserCapabilities = capabilities;
+
+
         SelenideLogger.addListener("AllureSelenide",
                 new AllureSelenide().screenshots(true).savePageSource(false));
 
